008ad0b5897a162f1824f1ca780123dbf7f880d8
lucas f silva <lucasdotfs@gmail.com>
2022-10-05 19:57:22 -0300

    Setando as rotas no App

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,17 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Global } from "../../assets/styles";
+import Home from "../../pages/Home";
 
 export default function App() {
-  return <div>PROJETO 10</div>;
+  return (
+    <>
+      <Global />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
---
+++ src/pages/Movie/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Container } from "./styles";
+
+export default function Movie() {
+  return <Container></Container>;
+}
